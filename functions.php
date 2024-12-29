47fbc61555985a40f826dd311f97c65832bcc85b
Micah Shumaker <78711034+micahshu@users.noreply.github.com>
2024-12-28 21:19:55 -0700

    Update functions.php
    added function to register the post  type to fix the url structure

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
  * https://github.com/timber/starter-theme
  */
 
+
+
 // Load Composer dependencies.
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -11,8 +13,28 @@ require_once __DIR__ . '/src/StarterSite.php';
 
 Timber\Timber::init();
 
+
+
 // Sets the directories (inside your theme) to find .twig files.
 Timber::$dirname = [ 'templates', 'views' ];
 
 new StarterSite();
 
+function modify_project_post_type() {
+    register_post_type('project',
+        array(
+            'labels' => array(
+                'name' => __('Projects'),
+                'singular_name' => __('Project')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array(
+                'slug' => 'project',
+                'with_front' => false  // This is the key setting
+            ),
+            // ... future post type settings
+        )
+    );
+}
+add_action('init', 'modify_project_post_type');
